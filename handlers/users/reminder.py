fe84678d0ba0567db4b4723a537194031e4a3390
Sanjar Alimjanov <145479374+MrASErDev@users.noreply.github.com>
2025-03-12 08:40:05 +0500

    Update reminder.py

--- handlers/users/reminder.py
+++ handlers/users/reminder.py
@@ -61,7 +61,7 @@ async def send_daily_reminder():
 
                 # Send the reminder message to the group
                 await bot.send_message(
-                    int(GROUPS[0:2]), 
+                    int(GROUPS[0]), 
                     f"<i>Assalomu aleykum</i>\n"
                     f"🗓 {sana} sanasi kuni,\n"
                     f"🫡 <b>{mention_link}</b>aka navbatchilikka siz tayinlangansiz!\n"
